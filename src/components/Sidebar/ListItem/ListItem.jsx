09595c4182bb569385b634bd15cbabd01976e490
tsankovelichkov <80650325+tsankovelichkov@users.noreply.github.com>
2022-02-13 16:18:52 +0200

    creating Users Component

--- src/components/Sidebar/ListItem/ListItem.jsx
+++ src/components/Sidebar/ListItem/ListItem.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import "./ListItem.css"
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ViewListRoundedIcon from '@mui/icons-material/ViewListRounded';
 
 const ListItem = () => {
     return (
         <div className="sidebar-quickMenu-container">
             <h2 className='quickMenu-title'>Quick Menu</h2>
             <ul className="sidebar-listItem">
-                <li className='sidebar-listItem-el'>Users</li>
-                <li className='sidebar-listItem-el'>Add Movie</li>
-                <li className='sidebar-listItem-el'>Add List</li>
+                <li className='sidebar-listItem-el active'>
+                    <AccountBoxIcon className='sidebar-icons'/>
+                    Users
+                </li>
+                <li className='sidebar-listItem-el'>
+                    <AddBoxIcon className='sidebar-icons'/>
+                    Add Movie
+                </li>
+                <li className='sidebar-listItem-el'>
+                    <ViewListRoundedIcon className='sidebar-icons'/>
+                    Add List
+                </li>
             </ul>
         </div>
     )
