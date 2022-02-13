0368689604eae860d489d53006fad8aef6b1b083
tsankovelichkov <80650325+tsankovelichkov@users.noreply.github.com>
2022-02-13 23:17:22 +0200

    updating Sidebar Component

--- src/components/Sidebar/ListItem/ListItem.jsx
+++ src/components/Sidebar/ListItem/ListItem.jsx
@@ -4,29 +4,58 @@ import { Link } from "react-router-dom";
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import ViewListRoundedIcon from '@mui/icons-material/ViewListRounded';
+import LocalMoviesRoundedIcon from '@mui/icons-material/LocalMoviesRounded';
+import PlaylistAddRoundedIcon from '@mui/icons-material/PlaylistAddRounded';
 
 const ListItem = () => {
     return (
-        <div className="sidebar-quickMenu-container">
-            <h2 className='quickMenu-title'>Quick Menu</h2>
-            <ul className="sidebar-listItem">
-                <Link className="link-button" to="/users">
-                    <li className='sidebar-listItem-el active'>
-                    <AccountBoxIcon className='sidebar-icons' />
-                    Users
-                    </li>
-                </Link>
-
-                <li className='sidebar-listItem-el'>
-                    <AddBoxIcon className='sidebar-icons' />
-                    Add Movie
-                </li>
-                <li className='sidebar-listItem-el'>
-                    <ViewListRoundedIcon className='sidebar-icons' />
-                    Add List
-                </li>
-            </ul>
-        </div>
+        <>
+            <div className="sidebar-quickMenu-container">
+                <h2 className='quickMenu-title'>Users</h2>
+                <ul className="sidebar-listItem">
+                    <Link className="link-button" to="/users">
+                        <li className='sidebar-listItem-el active'>
+                            <AccountBoxIcon className='sidebar-icons' />
+                            Users
+                        </li>
+                    </Link>
+                </ul>
+            </div>
+            <div className="sidebar-quickMenu-container">
+                <h2 className='quickMenu-title'>See Movies</h2>
+                <ul className="sidebar-listItem">
+                    <Link className="link-button" to="/movies">
+                        <li className='sidebar-listItem-el'>
+                            <LocalMoviesRoundedIcon className='sidebar-icons' />
+                            Movies
+                        </li>
+                    </Link>
+                    <Link className="link-button" to="/add-movie">
+                        <li className='sidebar-listItem-el'>
+                            <AddBoxIcon className='sidebar-icons' />
+                            Add Movie
+                        </li>
+                    </Link>
+                </ul>
+            </div>
+            <div className="sidebar-quickMenu-container">
+                <h2 className='quickMenu-title'>Lists</h2>
+                <ul className="sidebar-listItem">
+                <Link className="link-button" to="/add-list">
+                        <li className='sidebar-listItem-el'>
+                            <ViewListRoundedIcon className='sidebar-icons' />
+                            See List
+                        </li>
+                    </Link>
+                    <Link className="link-button" to="/add-list">
+                        <li className='sidebar-listItem-el'>
+                            <PlaylistAddRoundedIcon className='sidebar-icons' />
+                            Add List
+                        </li>
+                    </Link>
+                </ul>
+            </div>
+        </>
     )
 }
 
